cea4b062247fbf62a4894a84288ee8cbd8c950fb
www-data <koen.veys@protonmail.com>
2019-10-10 17:11:32 +0200

    reworked script to incorporate choices: show namespaces, create a namespace, delte a namespace

--- namespace.sh
+++ namespace.sh
@@ -9,19 +9,14 @@ bindir=$dokudir/bin
 nsdir=$dokudir/data/pages
 mediadir=$dokudir/data/media
 dwsite="https://dwkoen.ddns.net"
+currentns=$(find $nsdir -maxdepth 1 -type d ! -name '_template' -printf %f\\n |sort)
 
+# Setting functions
 
-################
-# start script #
-################
+#create a namespace
+CreateNamespace(){
 
-clear
-
-
-echo "           #################################"
-echo "           # DokuWiki: creating  namespace #"
-echo -e "           #################################\n"
-# ask for name of new namespace
+# name of new namespace
 echo "Name of your new namespace (ONLY underscore allowed!):"
 read nsname
 
@@ -42,3 +37,47 @@ echo "   https://www.google.com/search?as_st=y&tbm=isch&as_q=$nsname&as_epq=&as_
 echo "2) save the picture as: icon.jpg on your desktop" 
 echo -e "3) open a browser: $dwsite/doku.php?id=$nsname:$nsname" 
 echo -e "4) replace the existing icon.jpg with the one on your desktop.\n"
+
+}
+
+#delete a namespace
+DeleteNamespace(){
+
+# name of new namespace
+echo "Name of your namespace you want to delete:"
+echo $currentns
+read nsnameD
+
+for nsdel in $nsnameD; do
+
+	rm -rf $nsdir/$nsdel
+	rm -rf $mediadir/$nsdel
+done
+
+}
+
+################
+# start script #
+################
+
+clear
+
+
+echo "           ##################################"
+echo "           # DokuWiki: managing  namespaces #"
+echo -e "           ##################################\n"
+
+echo -e "Select the operation:\n"
+echo "1: display current namespaces"
+echo "2: create a new namespace"
+echo -e "3: delete a namespace\n"
+
+read action
+case $action in 
+	"1") for ns in $currentns; do echo $ns; done|nl
+	;;
+	"2") CreateNamespace 
+	;;
+	"3") DeleteNamespace
+	;;
+esac
